f6f145d06bf267c7bb8385218687c1a725931ea2
Hyunhong Kim <pol4bear@gmail.com>
2020-01-27 19:15:03 +0900

    Update main.cpp

--- Src/main.cpp
+++ Src/main.cpp
@@ -7,7 +7,6 @@ using namespace pol4b;
 
 int main(int argc, char *argv[]) {
     LogManager log_manager(argv[0]);
-
     int input_queue = 0;
     int output_queue = 1;
     Ip destination_ip;
@@ -17,9 +16,9 @@ int main(int argc, char *argv[]) {
     case 3: destination_ip = Ip(argv[1]); destination_port = atoi(argv[2]); break;
     default: LOG(ERROR) << "Usage: " << argv[0] << "[Destination IP] [Destination Port] [Input Queue Number] [Output Queue Number]"; exit(0);
     }
-
     IpChanger ip_changer(LogManager::on_error, LogManager::on_info);
     ip_changer.start(input_queue, output_queue, destination_ip, destination_port);
-
+    LogManager::on_info("IpChanger Started");
     while (ip_changer.is_started());
+    LogManager::on_info("IpChanger Exited");
 }
