9ebe4614a6b0d45b47fae5d6b26c16bc40b334d3
Hyunhong Kim <pol4bear@gmail.com>
2020-01-27 17:50:15 +0900

    Add Start Scripts

---
+++ set_netfilter_queue.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+$QUEUE_NUM
+
+INPUT_QUEUE_NUM=0
+OUTPUT_QUEUE_NUM=1
+if [ $# -eq 2 ]
+then
+	$INPUT_QUEUE_NUM = $1
+	$OUTPUT_QUEUE_NUM = $2
+elif [ $# -ne 0 ]
+then
+	echo "Usage: ${0} [Input Queue Number] [Output Queue Number]"
+	exit 1
+fi
+
+sudo iptables -F
+sudo iptables -A INPUT -j NFQUEUE --queue-num $INPUT_QUEUE_NUM
+sudo iptables -A OUTPUT -j NFQUEUE --queue-num $OUTPUT_QUEUE_NUM
+
